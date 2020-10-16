22a3572a1cc4ada2dfc8b3dcb166b9642e37de24
Benjamin Kim <benkim96@gmail.com>
2020-10-15 18:34:43 -0700

    create prod scripts

---
+++ test/prod_scripts/GET.sh
@@ -0,0 +1,3 @@
+curl --header "Content-Type: application/json" \
+	--request GET \
+	http://54.153.13.255/1
---
+++ test/prod_scripts/POST.sh
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" \
+	--request POST \
+	--data '{"url":"url.com"}' \
+	http://54.153.13.255
